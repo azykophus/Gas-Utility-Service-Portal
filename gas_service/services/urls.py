30666e3f74b2d460a7a399ab5f52dcff63dce818
Abhijay Singh <98527223+azykophus@users.noreply.github.com>
2024-03-21 05:09:22 +0530

    Delete gas_service directory

--- gas_service/services/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('submit/', views.submit_request, name='submit_request'),
-    path('track/', views.track_requests, name='track_requests'),
-]
